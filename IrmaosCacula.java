e9dcbfa5843e0efdfa94f9d7c29dff5717382e74
Pedro Queiroga <90732961+PedroQueiroga9@users.noreply.github.com>
2023-04-04 11:23:00 -0300

    Update IrmaosCacula.java

--- IrmaosCacula.java
+++ IrmaosCacula.java
@@ -1,9 +1,26 @@
 
-public class IrmaosCacula {
+import java.util.Scanner;
+public class Combustivel {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Informe a idade de João: ");
+		int joao = sc.nextInt();
+		System.out.println("Informe a idade de jose: ");
+		int jose = sc.nextInt();
+		System.out.println("Informe a idade de Maria");
+		int maria = sc.nextInt();
+		if ((joao < jose) && (joao < maria)) {
+			System.out.println("Joao é o caçula");	
+		}
+		else if ((jose < joao )&& (jose < maria)); {
+			System.out.println("José é o caçula");	
+		}
+		else {
+			System.out.println("Maria é a caçula");
+		}
+		sc.close();
 	}
 
 }
