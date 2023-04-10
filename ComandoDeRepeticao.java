5c2f886689a1fc8b9f2a1de559047f2f5c7d45fb
Pedro Queiroga <90732961+PedroQueiroga9@users.noreply.github.com>
2023-04-10 10:53:21 -0300

    Add files via upload

---
+++ ComandoDeRepeticao.java
@@ -0,0 +1,13 @@
+
+public class ComandoDeRepeticao {
+
+	public static void main(String[] args) {
+	
+		int x = 1;
+		while (x <= 3) {
+			System.out.println("Brasil ");
+			x = x + 1;
+		}
+	}
+
+}
