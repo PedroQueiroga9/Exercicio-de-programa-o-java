ddde82fc4ab987a18893c8eb0bdbcdd41e718da8
Pedro Queiroga <90732961+PedroQueiroga9@users.noreply.github.com>
2023-04-10 10:15:45 -0300

    Add files via upload

---
+++ FormasDeFormatacao.java
@@ -0,0 +1,13 @@
+
+public class FormasDeFormatacao {
+
+	public static void main(String[] args) {
+		String nome = "Pedro";
+		int idade = 18;
+		double x = 13.42342;
+		System.out.printf("Seu nome é %s, sua idade é %d e o x tem valor de %.2f", nome,idade,x);
+		
+		
+	}
+
+}
