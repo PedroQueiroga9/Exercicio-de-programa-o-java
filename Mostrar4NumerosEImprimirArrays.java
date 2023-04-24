dafa925d6e1b6d2f75c1bace1b30561e4ce92127
Pedro Queiroga <90732961+PedroQueiroga9@users.noreply.github.com>
2023-04-24 18:56:47 -0300

    Update Mostrar4NumerosEImprimirArrays.java

--- Mostrar4NumerosEImprimirArrays.java
+++ Mostrar4NumerosEImprimirArrays.java
@@ -2,7 +2,19 @@ import java.util.Scanner;
 public class Mostrar4NumerosEImprimirArrays {
 
 	public static void main(String[] args) {
+				Scanner sc = new Scanner(System.in);
+		int A [] = new int [5];
 		
+		for (int i = 0; i <= 3; i++) {
+			System.out.println("Informe um valor: ");
+			A[i] = sc.nextInt();
+		}
+		
+		for (int i = 0; i <= 3; i++) {
+			System.out.println("Posição	[" + i + "]" + A[i]);
+		}
+		
+		sc.close();
 
 	}
 
